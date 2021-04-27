b986483f1c7e8a466d78910405b487f021bf28c7
jahidanowar <milon5143@gmail.com>
2021-04-27 07:09:01 +0530

    Route Changed

--- routes/resourceRoute.js
+++ routes/resourceRoute.js
@@ -2,10 +2,12 @@ const express = require("express");
 const route = express.Router();
 const resourceController = require("./../controllers/resourceController");
 
+//Resources Route
 router
   .route("resource")
   .get(resourceController.index)
   .post(resourceController.store);
+//Resource Route
 router
   .route("resource/:slug")
   .get(resourceController.show)
