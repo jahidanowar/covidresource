3ad1dea0fbe2afd00cb2530bfd39f0d216654590
jahidanowar <milon5143@gmail.com>
2021-04-27 12:59:02 +0530

    Resource Model Updated

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const app = express();
+const resourceRoute = require("./routes/resourceRoute");
+
+
+/**
+ * Routes Registration
+ */
+app.use("api/v1/", resourceRoute);
+
+/**
+ * Unknown Route
+ */
+app.all("*", (req, res) => {
+  res.status(404).json(`${req.originalUrl} does not exists.`);
+});
 
 module.exports = app;
