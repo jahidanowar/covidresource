d711a89c6c3ed3a0128411c5c8cbc748c920ceae
jahidanowar <milon5143@gmail.com>
2021-04-27 13:24:43 +0530

    Routes Updated & Error Handling Functions Added

--- routes/resourceRoute.js
+++ routes/resourceRoute.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const route = express.Router();
+const router = express.Router();
 const resourceController = require("./../controllers/resourceController");
 
 //Resources Route
 router
-  .route("resource")
+  .route("/resource/")
   .get(resourceController.index)
   .post(resourceController.store);
 //Resource Route
 router
-  .route("resource/:slug")
+  .route("/resource/:slug/")
   .get(resourceController.show)
   .patch(resourceController.update);
+
+module.exports = router;
