fc1a717a2a9004ca2032da4025ef4447b498848c
jahidanowar <milon5143@gmail.com>
2021-04-28 10:20:38 +0530

    Index

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="sm:py-5 px-4 sm:px-0">
     <div class="my-8">
-      <input type="text" class="w-full shadow-lg p-4 rounded dark:bg-gray-700" :placeholder="$t('serach')">
+      <input
+        type="text"
+        class="w-full shadow-md p-4 rounded dark:bg-gray-700 focus:shadow-lg focus:outline-none"
+        :placeholder="$t('serach')"
+      />
     </div>
-    <div class="grid sm:grid-cols-2 gap-10">
+    <div class="grid sm:grid-cols-2 gap-5">
       <card-resource v-for="i in 10" :key="i" />
     </div>
   </div>
