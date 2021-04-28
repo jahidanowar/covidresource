8b6211b50296ad81b3a3da0e12972f2f8351fa03
jahidanowar <milon5143@gmail.com>
2021-04-28 21:33:25 +0530

    Static Changed to Server

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,7 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "server",
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "CovidResource",
