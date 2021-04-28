116129a1035b01a8cd41d51f2d4870112248975c
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-04-27 23:14:14 -0700

    add coordinate loc and complete show route

--- backend/controllers/resourceController.js
+++ backend/controllers/resourceController.js
@@ -14,6 +14,8 @@ exports.store = catchAsync(async(req, res) => {
   const state = req.body.state;
   const district = req.body.district ;
   const address = req.body.address;
+  const long = req.body.long;
+  const lat = req.body.lat;
 
   const resource = new Resource({
     category : category,
@@ -21,14 +23,22 @@ exports.store = catchAsync(async(req, res) => {
     link: link,
     state: state,
     district: district,
-    address: address
+    address: address,
+    location :{
+      type: "Point",
+      coordinates: [long, lat]
+    }
   });
   const source = await resource.save();
   res.status(200).json({resource: source});
 });
 
-exports.show = (req, res) => {};
+exports.show =catchAsync(async(req, res) => {
+  const resourceId = req.params.slug ;
+
+  const resource = await Resource.findById(resourceId);
+  res.status(200).json({data: resource});
+});
 
 exports.update = (req, res) => {};
 
-exports.destroy = (req, res) => {};
