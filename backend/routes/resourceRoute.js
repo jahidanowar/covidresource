3ccc1244cc34e15d755512d63da66bb108ee1ee2
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-04-28 18:00:01 -0700

    add cacheMiddleWare

--- backend/routes/resourceRoute.js
+++ backend/routes/resourceRoute.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const resourceController = require("./../controllers/resourceController");
+const cache = require('../middleware/cacheMiddleware');
 
 //Resources Route
 router
   .route("/resource/")
-  .get(resourceController.index)
+  .get([cache.cacheMiddleware(30)],resourceController.index)
   .post(resourceController.store);
 //Resource Route
 router
   .route("/resource/:slug/")
-  .get(resourceController.show)
+  .get([cache.cacheMiddleware(30)],resourceController.show)
   .patch(resourceController.update);
 
 module.exports = router;
