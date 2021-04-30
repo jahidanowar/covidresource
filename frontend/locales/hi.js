c4ec3939075d71816f2485bdddd168a80d4801ed
jahidanowar <milon5143@gmail.com>
2021-04-30 11:56:56 +0530

    Hindi language file created

---
+++ frontend/locales/hi.js
@@ -0,0 +1,35 @@
+export default {
+  welcome: "Hello World",
+  logo: {
+    covid: "Covid",
+    resource: "Resource"
+  },
+  dark: "Dark",
+  light: "Light",
+  serach: "Search",
+  appbar: {
+    home: "HOME",
+    map: "MAP",
+    tiles: "LIST",
+    add: "ADD",
+    menu: "MENU",
+    close: "CLOSE"
+  },
+  drawer: {
+    support: "Support",
+    contribute: "Contribute",
+    about: "About us"
+  },
+  addResource: {
+    title: "Be a Hero",
+    subtitle: "Help other by adding verified resources",
+    category: "Category",
+    name: "Name",
+    phone: "Phone",
+    state: "State",
+    district: "District",
+    address: "Address",
+    add: "Add Resource",
+    searchCategory: "What are you looking for"
+  }
+};
