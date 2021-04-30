036977b88e95892e8a997ac0841500a69e86b496
jahidanowar <milon5143@gmail.com>
2021-04-30 06:18:56 +0530

    Only Localhost restriction added

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "127.0.0.1", () => {
   console.log(`App is running on ${PORT}`);
 });
