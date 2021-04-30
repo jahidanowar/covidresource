3e91621f11f6d61ef2f2204dbff771115043e998
jahidanowar <milon5143@gmail.com>
2021-04-30 06:33:30 +0530

    API env Added 🚀

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -77,7 +77,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: "http://localhost:5000/api/v1"
+    baseURL: process.env.API_URL || "http://localhost:5000/api/v1"
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
