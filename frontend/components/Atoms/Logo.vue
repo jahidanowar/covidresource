86325c791ddd90bd0cfd15fe539b2d9e6992c104
jahidanowar <milon5143@gmail.com>
2021-04-30 19:16:03 +0530

    Beta Mark added to Logo

--- frontend/components/Atoms/Logo.vue
+++ frontend/components/Atoms/Logo.vue
@@ -2,6 +2,7 @@
   <nuxt-link to="/">
     <h2 class="text-2xl font-bold">
       <span class="opacity-80">{{ $t("logo.covid") }}</span>{{ $t("logo.resource") }}
+      <sup class="text-xs font-normal opacity-70">beta</sup>
     </h2>
   </nuxt-link>
 </template>
