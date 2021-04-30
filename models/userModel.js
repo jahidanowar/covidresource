fb60f690584f09527a489482158aeb8cccb27675
jahidanowar <milon5143@gmail.com>
2021-04-30 16:49:30 +0530

    Models Updated

--- models/userModel.js
+++ models/userModel.js
@@ -2,18 +2,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email:{
-      type:String,
-      required:true
+  email: {
+    type: String,
+    required: true,
   },
-  username:{
-      type:String,
-      required:true
+  password: {
+    type: String,
+    required: true,
   },
-  password:{
-      type:String,
-      required:true
-  }
+  name: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["volunteer", "moderator"],
+    default: "volunteer",
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
