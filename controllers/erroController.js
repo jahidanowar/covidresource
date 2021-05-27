370b1e77e9d6c4bbe78e72c474b57f9f462e06e9
jahidanowar <milon5143@gmail.com>
2021-05-27 10:39:34 +0530

    Better Comment Added

--- controllers/erroController.js
+++ controllers/erroController.js
@@ -1,3 +1,12 @@
+
+/**
+ *  Global Error Handler
+ * @param {*} err 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @returns void
+ */
 module.exports = (err, req, res, next) => {
   res.status(err.statusCode || 500).json({
     status: err.status || "error",
