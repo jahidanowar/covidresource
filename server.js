067ee168b65ae4c1ccb6a9c1647b9bd6fc3a4558
jahidanowar <milon5143@gmail.com>
2021-06-03 08:25:14 +0530

    fixes: server

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, () => {
   console.log(`App is running on ${PORT}`);
 });
